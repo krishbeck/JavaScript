15681303d265ac2127ce5ed53f3b8641cc8e943a
Krishna Yadav <krishbeck@yahoo.com>
2023-10-02 15:24:36 -0500

    Updated script source in html file and added functions in index.js file

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Hello Krishna!!")
+
+
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0
+
+function increment(){
+    count +=1;
+    countEl.innerText = count
+
+}
+
+function save(){
+    console.log("Save Button Clicked")
+}
