2e0a3e0ecc10c56cc12b0b8781f04ee73cc0c6de
Krishna Yadav <krishbeck@yahoo.com>
2023-10-02 15:57:23 -0500

    Updated innerText to textContect in index.js

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ console.log("Hello Krishna!!")
 
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 console.log(countEl)
+console.log(saveEl)
 
 let count = 0
 
 function increment(){
     count +=1;
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
     console.log("Save Button Clicked")
 }
